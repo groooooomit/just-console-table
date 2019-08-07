9b162c3be27943bd4252cfe01adb0ede4fc9f3a7
fubo <groooooomit@gmail.com>
2019-08-07 19:04:04 +0800

    Add upload to jcenter configuration.

--- JustConsoleTable/just_console_table/src/test/java/just/console/table/ExampleUnitTest.java
+++ JustConsoleTable/just_console_table/src/test/java/just/console/table/ExampleUnitTest.java
@@ -2,7 +2,9 @@ package just.console.table;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,15 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testTable() {
+        String table = Table.from("老百姓", Arrays.asList("张三", "李四", "王五", "赵六", "钱七"));
+        System.out.println(table);
     }
 }
